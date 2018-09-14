53253f32e9544bb5e6d75bcecd3eb9d54a1e9141
Zerovers <Zeromon19941994@mail.ru>
2018-09-14 09:47:55 +0300

    2task

--- src/index.js
+++ src/index.js
@@ -3,5 +3,21 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
+  var first;
+  var second;
+  var third;
+  var count = 0;
+  for ( i = 0; i < preferences.length - 1; i++) {
+    first = preferences[i];
+    second = preferences[first -1];
+    third = preferences[second -1];
+    if( third == i + 1 && first !== second && second !== third ) {
+      count++;
+      preferences[i] = undefined;
+      preferences[first - 1] = undefined;
+      preferences[second -1] = undefined;
+    }
+  }
+  return count;
   // your implementation
 };
